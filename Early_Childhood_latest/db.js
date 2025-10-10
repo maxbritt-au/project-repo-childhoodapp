2ed9d0ad6c36a009159dcb64610e6b129dbe439b
Andrew <andrew@Andrews-MacBook-Pro.local>
2025-10-10 13:39:51 +1100

    fix some bugs for deployment

--- Early_Childhood_latest/db.js
+++ Early_Childhood_latest/db.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,       // must exist
-  user: process.env.DB_USER,       // must exist
-  password: process.env.DB_PASSWORD, // must exist
-  database: process.env.DB_NAME,   // must exist
-  port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect(err => {
   if (err) {
-    console.error("Error connecting to MySQL: ", err);
+    console.error("❌ Error connecting to MySQL: ", err);
     process.exit(1); // stop server if DB connection fails
+  } else {
+    console.log("✅ Connected to MySQL!");
   }
-  console.log("Connected to MySQL!");
 });
 
 module.exports = connection;
